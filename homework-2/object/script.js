32cea3cd93dd61aa2058729cd9160374c48f150b
Pablo <pasha.dramaretskiy@gmail.com>
2019-03-27 19:01:19 +0200

    Выполнил дз №2

---
+++ homework-2/object/script.js
@@ -0,0 +1,13 @@
+var object = {
+  model: "12",
+  color: "12"
+}
+
+var someObject = {
+  product: "iphone",
+  price: 1000,
+  currency: "dollar",
+  details: object
+};
+
+console.log(someObject);
