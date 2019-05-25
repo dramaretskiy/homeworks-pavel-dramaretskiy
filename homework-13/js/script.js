6987e60cccd0dcd13d36130b102ce46ad5ce395a
Pablo <pasha.dramaretskiy@gmail.com>
2019-05-25 12:20:55 +0300

    add-hm-13

---
+++ homework-13/js/script.js
@@ -0,0 +1,48 @@
+// 1. Создать функцию, которая возвращает промис.
+// Функция принимает два аргумента - время, через которое промис должен выполниться,
+// и значение, с которым промис будет выполнен.
+// function promiseCreator(...) {...}
+// const prom = promiseCreator(500, 'Ok!'); prom.then(console.log);
+// Ok!
+
+function promiseCreator(time, value) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(console.log(value));
+    }, time);
+  });
+}
+
+const prom = promiseCreator(2000, "Ok!");
+prom.then();
+
+// 2. Создать класс, который производит экземпляр со следующими свойствами:
+// - promise - промис, который создается во время запуска конструктора;
+// - reject - метод, при выполнении которого promise реджектится;
+// - resolve - метод, при выполнении которого promise резолвится.
+// class Prom {...}
+// const inst = new Prom();
+// inst.promise.then(data => console.log(data));
+// setTimeout(() => inst.resolve("test"), 5000);
+// → test
+
+class Prom {
+  constructor() {
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
+  }
+
+  resolve() {
+    return (this.promise = value);
+  }
+
+  reject() {
+    return (this.promise = value);
+  }
+}
+
+const inst = new Prom();
+inst.promise.then(data => console.log(data));
+setTimeout(() => inst.resolve("test"), 2000);
