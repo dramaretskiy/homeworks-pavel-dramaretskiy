58a06b2d0438e282b1d7fcd6ae7c552dcd0b628e
Pablo <pasha.dramaretskiy@gmail.com>
2019-06-06 11:06:31 +0300

    remove

--- help/src/js/config/config.js
+++
@@ -1,4 +0,0 @@
-export const config = {
-  apiUrl: 'https://newsapi.org/v2',
-  apiKey: '9c27b0f722b84da5a08312d2b125351b'
-}
--- help/src/js/view/uiElements.config.js
+++
@@ -1,7 +0,0 @@
-const form = document.forms["newsControlForm"];
-const countrySelect = document.forms["newsControlForm"]["country"];
-const categorySelect = document.forms["newsControlForm"]["category"];
-const newsContainer = document.querySelector(".news-wrap .row");
-const searchInput = document.forms["newsControlForm"]["search"];
-
-export { form, countrySelect, categorySelect, newsContainer, searchInput };
