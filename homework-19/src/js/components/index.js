bf12641a37bfb38bec7004225d14fb402130ccbf
Pablo <pasha.dramaretskiy@gmail.com>
2019-06-14 15:03:05 +0300

    add files

---
+++ homework-19/src/js/components/index.js
@@ -0,0 +1,7 @@
+export * from './home.component'
+export * from './login.component'
+export * from './news.component'
+export * from './notfound.component'
+export * from './signup.component'
+export * from './user.component'
+export * from './navbar.component'
---
+++ homework-19/src/js/services/index.js
@@ -0,0 +1,3 @@
+export * from './auth.service'
+export * from './news.service'
+export * from './user.service'
